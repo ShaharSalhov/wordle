cff6b39ba27e4eadb07b5969969ddd6df5e2d2c9
ShaharSalhov <shaharsalhov0@gmail.com>
2023-06-17 19:02:19 +0300

    completed tutorial and added onscreen keyboard

---
+++ src/components/Keypad.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react'
+
+export default function Keypad( {usedKeys, handleKeyup} ) {
+  const [letters, setLetters] = useState(null)
+
+  useEffect( () => {
+    fetch('http://localhost:3001/letters')
+      .then(res => res.json())
+      .then(json => {
+        setLetters(json)
+    })
+  }, [])
+
+  return (
+    <div className='keypad'>
+      {letters && letters.map((l) => {
+        const color = usedKeys[l.key]
+        return (
+          <button key={l.key} className={color} onClick={ () => (handleKeyup(l)) } >{l.key.toUpperCase()}</button>
+        )
+      })}
+      <button key="Delete" className='action' onClick={ () => handleKeyup({key: "Backspace"}) }>Delete</button>
+      <button key="Enter" className='action' onClick={ () => handleKeyup({key: "Enter"}) }>Enter</button>
+    </div>
+  )
+}
