a100702df87817fc1728eb0b6f516341e06b3acf
ShaharSalhov <shaharsalhov0@gmail.com>
2023-06-17 19:50:31 +0300

    replaced usage of json-server with json static files

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import { useState } from "react";
 import { useEffect } from "react";
 import Wordle from "./components/Wordle";
+import solutions from "./solutions.json"
+
 
 function App() {
   const [solution, setSolution] = useState(null)
 
   useEffect( () => {
-    fetch('http://localhost:3001/solutions')
-      .then(res => res.json())
-      .then(json => {
-        const randomSolution = json[Math.floor(Math.random()*json.length)]
-        setSolution(randomSolution.word)
-      })
+    // fetch('http://localhost:3001/solutions')
+    //   .then(res => res.json())
+    //   .then(json => {
+    //     const randomSolution = json[Math.floor(Math.random()*json.length)]
+    //     setSolution(randomSolution.word)
+    //   })
+    const randomSolution = solutions[Math.floor(Math.random() * solutions.length)]
+    setSolution(randomSolution.word)
   }, [setSolution])
 
   return (
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,15 +1,16 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
+import letters from "../letters.json"
 
 export default function Keypad( {usedKeys, handleKeyup} ) {
-  const [letters, setLetters] = useState(null)
+  // const [letters, setLetters] = useState(null)
 
-  useEffect( () => {
-    fetch('http://localhost:3001/letters')
-      .then(res => res.json())
-      .then(json => {
-        setLetters(json)
-    })
-  }, [])
+  // useEffect( () => {
+  //   fetch('http://localhost:3001/letters')
+  //     .then(res => res.json())
+  //     .then(json => {
+  //       setLetters(json)
+  //   })
+  // }, [])
 
   return (
     <div className='keypad'>
